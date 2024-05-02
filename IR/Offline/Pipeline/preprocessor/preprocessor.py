c6761052bc43d7a8fffb4491a54aa8c50b4d3b59
nicolas_alahmar <nicolasalahmar@gmail.com>
2024-05-02 23:54:24 +0300

    - matching and ranking - num2words

--- IR/Offline/Pipeline/preprocessor/preprocessor.py
+++ IR/Offline/Pipeline/preprocessor/preprocessor.py
@@ -1,3 +1,4 @@
+from Pipeline.preprocessor.num2words import convert_num2words
 from Pipeline.preprocessor.punctuation import remove_punctuation
 from Pipeline.preprocessor.spelling import correct_sentence_spelling
 from Pipeline.preprocessor.stemmer import stem
@@ -7,8 +8,8 @@ from Pipeline.preprocessor.tokenize import tokenize
 
 def preprocessor(text):
     tokens = tokenize(text)
-
-    unpunctuated_tokens = remove_punctuation(tokens)
+    num_handled_tokens = convert_num2words(tokens)
+    unpunctuated_tokens = remove_punctuation(num_handled_tokens)
     no_stop_words_tokens = remove_stopwords(unpunctuated_tokens)
     spell_checked_tokens = correct_sentence_spelling(no_stop_words_tokens)
     stemmed_tokens = stem(spell_checked_tokens)
--- IR/Offline/main.py
+++ IR/Offline/main.py
@@ -1,9 +1,13 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 from Helper.ORM import fetch_records
 from Pipeline.index.index import Index
 from datetime import datetime
+from Pipeline.preprocessor.preprocessor import preprocessor
 
 start = datetime.now()
-records = fetch_records('dataset1.db', 10)
+records = fetch_records('dataset1.db', 100)
+documents = [rec.text for rec in records]
 index = Index(records)
 index.save()
 end = datetime.now()
@@ -11,5 +15,24 @@ end = datetime.now()
 print(index.create_dataframe())
 print(end - start)
 
-# index = Index.load()
-# print(index.create_dataframe())
+index = Index.load()
+query = '1775'
+preprocessed_query = preprocessor(query)
+print(preprocessed_query)
+
+query_vector = index.vectorizer.transform([preprocessed_query])
+
+cosine_similarities = cosine_similarity(query_vector, index.tfidf_matrix)
+
+cosine_similarities = cosine_similarities.flatten()
+
+sorted_indices = np.argsort(cosine_similarities)[::-1]
+
+for i, idx in enumerate(sorted_indices):
+    print(f"Document {idx}: Similarity Score {cosine_similarities[idx]}")
+    print(documents[idx])
+
+top_n = 10
+top_documents = [(idx, cosine_similarities[idx]) for idx in sorted_indices[:top_n]]
+for idx, score in top_documents:
+    print(f"Document {idx}: Similarity Score {score}")
