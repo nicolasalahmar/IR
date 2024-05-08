fc8148664e8083e95f4a22f5a31bdc7efc7e642d
GeoN <geon200181@gmail.com>
2024-05-08 19:08:04 +0300

    Date To Text (Fix Bug)

--- IR/Offline/Pipeline/preprocessor/date_to_text.py
+++ IR/Offline/Pipeline/preprocessor/date_to_text.py
@@ -5,22 +5,23 @@ from num2words import num2words
 
 
 def convert_date_format(tokens):
+    date_pattern = r"(\d{1,2})[/-](\d{1,2})[/-](\d{4})"
 
-  date_pattern = r"(\d{1,2})[/-](\d{1,2})[/-](\d{4})"
+    for i, token in enumerate(tokens):
+        matches = re.findall(date_pattern, token)
+        for match in matches:
+            try:
+                month, day, year = match
+                month_name = datetime(year=int(year), month=int(month), day=1).strftime('%B').lower()
+                written_date = f"{month_name} {num2words(int(day), lang='en')}, {num2words(int(year), lang='en')}"
 
-  for i, token in enumerate(tokens):
-    matches = re.findall(date_pattern, token)
-    for match in matches:
-      month, day, year = match
-      month_name = datetime(year=int(year), month=int(month), day=1).strftime('%B').lower()
-      written_date = f"{month_name} {num2words(int(day), lang='en')}, {num2words(int(year), lang='en')}"
+                tokens[i] = re.sub(date_pattern, written_date, token, count=1).replace(' ', '')
+            except ValueError:
 
-      tokens[i] = re.sub(date_pattern, written_date, token, count=1).replace(' ','')
+                pass
 
-  return tokens
+    return tokens
 
-
-
-# text = ['2/2/1990','3-14-2024','hello']
-# converted_tokens = convert_date_format(text)
+# tokens = ["19-2-2022", "1-1-2024", "3-12-2025"]
+# converted_tokens = convert_date_format(tokens)
 # print(converted_tokens)
