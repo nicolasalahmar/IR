71b36be754138b3a995fd827a5fa22ffd514586f
nicolas_alahmar <nicolasalahmar@gmail.com>
2024-05-08 23:00:15 +0300

    date to text now handles m-d-y or d-m-y

--- IR/Offline/Pipeline/preprocessor/date_to_text.py
+++ IR/Offline/Pipeline/preprocessor/date_to_text.py
@@ -3,25 +3,33 @@ from datetime import datetime
 
 from num2words import num2words
 
+date_pattern = r"(\d{1,2})[/-](\d{1,2})[/-](\d{4})"
+
+
+def extract_date(match, token):
+    month, day, year = match
+    month_name = datetime(year=int(year), month=int(month), day=1).strftime('%B').lower()
+    written_date = f"{month_name} {num2words(int(day), lang='en')}, {num2words(int(year), lang='en')}"
+    return re.sub(date_pattern, written_date, token, count=1).replace(' ', '')
+
 
 def convert_date_format(tokens):
-    date_pattern = r"(\d{1,2})[/-](\d{1,2})[/-](\d{4})"
-
     for i, token in enumerate(tokens):
         matches = re.findall(date_pattern, token)
         for match in matches:
             try:
-                month, day, year = match
-                month_name = datetime(year=int(year), month=int(month), day=1).strftime('%B').lower()
-                written_date = f"{month_name} {num2words(int(day), lang='en')}, {num2words(int(year), lang='en')}"
-
-                tokens[i] = re.sub(date_pattern, written_date, token, count=1).replace(' ', '')
+                tokens[i] = extract_date(match, token)
             except ValueError:
-
-                pass
+                try:
+                    match = (match[1], match[0], match[2])
+                    tokens[i] = extract_date(match, token)
+                except Exception:
+                    pass
 
     return tokens
 
-# tokens = ["19-2-2022", "1-1-2024", "3-12-2025"]
-# converted_tokens = convert_date_format(tokens)
-# print(converted_tokens)
+
+if __name__ == '__main__':
+    tokens = ["19-2-2022", "1-1-2024", "3-12-2025"]
+    converted_tokens = convert_date_format(tokens)
+    print(converted_tokens)
