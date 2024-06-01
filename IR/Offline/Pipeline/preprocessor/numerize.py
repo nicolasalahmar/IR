943b38cd519903c5acbde6a46f8782e0c9939892
GeoN <geon200181@gmail.com>
2024-06-01 22:15:12 +0300

    Modify Numerize.py

--- IR/Offline/Pipeline/preprocessor/numerize.py
+++ IR/Offline/Pipeline/preprocessor/numerize.py
@@ -3,13 +3,33 @@ import numerizer
 from Offline.Pipeline.preprocessor import nlp
 
 
-def numerize_text(sentences):
-    corrected_sentences = []
-    for sentence in sentences:
-        try:
-            doc = nlp(sentence)
-            numerized_sentence = numerizer.numerize(doc.text)
-            corrected_sentences.append(numerized_sentence)
-        except ZeroDivisionError:
-            corrected_sentences.append(sentence)
-    return corrected_sentences
+def replace_number(text, entity, offset):
+    beg, end = entity.start_char, entity.end_char
+    try:
+        replacement = numerizer.numerize(text)
+    except ZeroDivisionError:
+        replacement = entity.text
+    except Exception:
+        replacement = entity.text
+    res = text[:beg + offset] + replacement + text[end + offset:]
+    og_len = len(entity.text)
+    new_len = len(replacement)
+    offset = (new_len - og_len) + offset
+
+    return res, offset
+
+def numerize_text(text):
+    nlp_text = nlp(text)
+    entities = nlp_text.ents
+
+    offset = 0
+    for entity in entities:
+        print(entity.label,entity.text)
+        if entity.label_ == 'CARDINAL':
+            text, offset = replace_number(text, entity, offset)
+
+    return text
+
+if __name__ == "__main__":
+    s = 'ten'
+    print(numerize_text(s))
